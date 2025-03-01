d770b976362b140c06f60437d2a1f7ca8b8b1230
Sheelam Vihas Reddy <vihasreddy111@gmail.com>
2025-03-01 14:50:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'returnright.info@gmail.com'
-app.config['MAIL_PASSWORD'] = "rqup ggrr zcmg bqjh"
-app.config['MAIL_DEFAULT_SENDER'] = 'returnright.info@gmail.com'
+app.config['MAIL_USERNAME'] = "Enter mail"
+app.config['MAIL_PASSWORD'] = "app password"
+app.config['MAIL_DEFAULT_SENDER'] = 'sender mail'
 mail = Mail(app)
 db = SQLAlchemy(app)
 
